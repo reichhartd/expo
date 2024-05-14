33c91ac9bbf9617599cfd7d1be1d8a21f12a8789
Daniel Reichhart <daniel.reichhart@gmx.net>
2024-05-14 20:47:22 +0200

    feat: draft

--- packages/expo-modules-core/src/EventEmitterHook.ts
+++ packages/expo-modules-core/src/EventEmitterHook.ts
@@ -8,9 +8,10 @@ interface EventEmitterHookMethods<TEvent> {
 }
 
 /**
- * Get or request permission for protected functionality within the app.
- * It uses separate permission requesters to interact with a single permission.
- * By default, the hook will only retrieve the permission status.
+ * A React hook that provides the current state of an event and the ability to listen for changes to that event.
+ * It initializes the state with an empty state and subscribes to the event to receive updates.
+ * @param methods - An object with methods for interacting with the event, including initialization and event listener.
+ * @returns The current state of the event.
  */
 function useEventEmitter<TEvent>(methods: EventEmitterHookMethods<TEvent>): TEvent {
   const [event, setEvent] = useState<TEvent>(methods.emptyState);
@@ -25,8 +26,10 @@ function useEventEmitter<TEvent>(methods: EventEmitterHookMethods<TEvent>): TEve
 }
 
 /**
- * Create a new permission hook with the permission methods built-in.
- * This can be used to quickly create specific permission hooks in every module.
+ * Creates a new hook for managing event emitter interactions specific to a particular type of event.
+ * This function is intended to simplify the creation of custom hooks for different modules using the same pattern.
+ * @param methods - The methods for event management and initial state setup.
+ * @returns A custom hook function that manages the event lifecycle and state.
  */
 export function createEventEmitterHook<TEvent>(methods: EventEmitterHookMethods<TEvent>) {
   return () => useEventEmitter<TEvent>(methods);
